a53e76bf9d743d3c4afd15d134882993212420ee
w3villa-Tarun022 <tarunkaumar.yadav@w3villa.com>
2022-03-11 11:31:30 +0530

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'admin', loadChildren:()=>import('./admin/admin.module').then(mod=>mod.AdminModule )
+  },
+  {
+    path:'user', loadChildren:()=>import('./user/user.module').then(mod=>mod.UserModule )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; '@angular/form'; 
 import { AdminModule } from './admin/admin.module'
+import { MenuModule } from './header/menu.module'; './header/MenuModule'
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { AdminModule } from './admin/admin.module'
     BrowserModule,
     FormsModule,
     AdminModule,
+    MenuModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
